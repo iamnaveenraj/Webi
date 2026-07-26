b61b55c0e55de25847942c1f0d1564b940925f21
Naveen Kumar <naveen.kumar2@naveenku-ltmy2k4.internal.salesforce.com>
2026-07-26 21:27:56 +0530

    first commit5

--- Websi/assets/js/script.js
+++ Websi/assets/js/script.js
@@ -384,39 +384,3 @@ form.addEventListener("submit", async (e) => {
     form.reset();
 
 });
-
-const hamburger = document.getElementById("hamburger");
-const navMenu = document.getElementById("navMenu");
-
-if (hamburger && navMenu) {
-
-    hamburger.addEventListener("click", () => {
-
-        navMenu.classList.toggle("active");
-
-        const icon = hamburger.querySelector("i");
-
-        if (navMenu.classList.contains("active")) {
-            icon.classList.remove("fa-bars");
-            icon.classList.add("fa-times");
-        } else {
-            icon.classList.remove("fa-times");
-            icon.classList.add("fa-bars");
-        }
-    });
-
-    document.querySelectorAll("#navMenu a").forEach(link => {
-
-        link.addEventListener("click", () => {
-
-            navMenu.classList.remove("active");
-
-            const icon = hamburger.querySelector("i");
-            icon.classList.remove("fa-times");
-            icon.classList.add("fa-bars");
-
-        });
-
-    });
-
-}
