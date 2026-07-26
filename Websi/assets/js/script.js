8e1403ca749723cfa82cbd6e24c6b1bb2fe48b7c
Naveen Kumar <naveen.kumar2@naveenku-ltmy2k4.internal.salesforce.com>
2026-07-26 19:38:13 +0530

    first commit5

--- Websi/assets/js/script.js
+++ Websi/assets/js/script.js
@@ -388,13 +388,35 @@ form.addEventListener("submit", async (e) => {
 const hamburger = document.getElementById("hamburger");
 const navMenu = document.getElementById("navMenu");
 
-hamburger.addEventListener("click", function () {
-    navMenu.classList.toggle("active");
-});
+if (hamburger && navMenu) {
 
-// Close menu after clicking a page
-document.querySelectorAll("#navMenu a").forEach(link => {
-    link.addEventListener("click", function () {
-        navMenu.classList.remove("active");
+    hamburger.addEventListener("click", () => {
+
+        navMenu.classList.toggle("active");
+
+        const icon = hamburger.querySelector("i");
+
+        if (navMenu.classList.contains("active")) {
+            icon.classList.remove("fa-bars");
+            icon.classList.add("fa-times");
+        } else {
+            icon.classList.remove("fa-times");
+            icon.classList.add("fa-bars");
+        }
     });
-});
+
+    document.querySelectorAll("#navMenu a").forEach(link => {
+
+        link.addEventListener("click", () => {
+
+            navMenu.classList.remove("active");
+
+            const icon = hamburger.querySelector("i");
+            icon.classList.remove("fa-times");
+            icon.classList.add("fa-bars");
+
+        });
+
+    });
+
+}
